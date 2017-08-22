a5303a821deb4c841367de9cf89c55cdea7cdaa4
Widyartini <Widyartini@smart-tbk.com>
2017-08-22 16:53:13 +0700

    app

---
+++ app/src/main/java/dicoding/tutorial/myalarmmanager/AlarmPreference.java
@@ -0,0 +1,14 @@
+package dicoding.tutorial.myalarmmanager;
+
+/**
+ * Created by Widyartini on 8/22/2017.
+ */
+
+public class AlarmPreference {
+    private final String PREF_NAME = "AlarmPreference";
+    private final String KEY_ONE_TIME_DATE = "onTimeDate";
+    private final String KEY_ONE_TIME_TIME = "oneTimeTime";
+    private final String KEY_ONE_TIME_MESSAGE = "oneTimeMessage";
+    private final String KEY_REPEATING_MESSAGE = "repeatingTime";
+    
+}
