22a1e9a09c934897dde9463b172c3ebfcd18f657
Widyartini <Widyartini@smart-tbk.com>
2017-08-28 16:42:22 +0700

    added CommitTest.txt to the repo

--- app/src/main/java/dicoding/tutorial/myalarmmanager/AlarmPreference.java
+++ app/src/main/java/dicoding/tutorial/myalarmmanager/AlarmPreference.java
@@ -1,5 +1,8 @@
 package dicoding.tutorial.myalarmmanager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by Widyartini on 8/22/2017.
  */
@@ -9,6 +12,63 @@ public class AlarmPreference {
     private final String KEY_ONE_TIME_DATE = "onTimeDate";
     private final String KEY_ONE_TIME_TIME = "oneTimeTime";
     private final String KEY_ONE_TIME_MESSAGE = "oneTimeMessage";
-    private final String KEY_REPEATING_MESSAGE = "repeatingTime";
-    
+    private final String KEY_REPEATING_TIME = "repeatingTime";
+    private final String KEY_REPEATING_MESSAGE = "repeatingMessage";
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    public AlarmPreference(Context context) {
+        mSharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = mSharedPreferences.edit();
+    }
+
+    public void setOneTimeDate(String date) {
+        editor.putString(KEY_ONE_TIME_DATE, date);
+        editor.commit();
+    }
+
+    public String getOneTimeDate() {
+        return mSharedPreferences.getString(KEY_ONE_TIME_DATE, null);
+    }
+
+    public void setOneTimeTime(String time) {
+        editor.putString(KEY_ONE_TIME_TIME, time);
+        editor.commit();
+    }
+
+    public String getOneTimeTime() {
+        return mSharedPreferences.getString(KEY_ONE_TIME_TIME, null);
+    }
+
+    public void setOneTimeMessage(String message){
+        editor.putString(KEY_ONE_TIME_MESSAGE, message);
+        editor.commit();
+    }
+
+    public String getOneTimeMessage(){
+        return mSharedPreferences.getString(KEY_ONE_TIME_MESSAGE, null);
+    }
+
+    public void setRepeatingTime(String time) {
+        editor.putString(KEY_REPEATING_TIME, time);
+        editor.commit();
+    }
+
+    public String getRepeatingTime() {
+        return mSharedPreferences.getString(KEY_REPEATING_TIME, null);
+    }
+
+    public void setRepeatingMessage(String message){
+        editor.putString(KEY_REPEATING_MESSAGE, message);
+    }
+
+    public String getRepeatingMessage(){
+        return mSharedPreferences.getString(KEY_REPEATING_MESSAGE, null);
+    }
+
+    public void clear(){
+        editor.clear();
+        editor.commit();
+    }
+
 }
